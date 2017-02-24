0b1712f770dc913347fe72085e100d2c8c540f64
Boris Estrin <borisestrin@gmail.com>
2017-02-24 21:12:21 +0200

    Initial Commit

---
+++ TwitterSearchSampleObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterSearchSampleObjC
+//
+//  Created by Admin on 24/02/2017.
+//  Copyright © 2017 Tel-Ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterSearchSampleObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterSearchSampleObjC
+//
+//  Created by Admin on 24/02/2017.
+//  Copyright © 2017 Tel-Ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
